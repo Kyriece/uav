848c0c936d25117954ae4beaefd7703d0c0ead3f
Kyriece <kyriece@hotmail.com>
2023-06-15 18:35:51 +1000

    Object tracking working and implemented

--- scripts/objectTracking.py
+++ scripts/objectTracking.py
@@ -7,15 +7,102 @@ from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
 import signal
 from mavros_msgs.srv import CommandBool, SetMode, SetModeRequest, CommandBoolRequest, CommandTOL
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image, Range
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
 
 current_state = State()
 velocity_pub = None
-takeoff_altitude = 3
+takeoff_altitude = 5
+
+horizontal_speed = 0.5  # Horizontal speed when correcting position
+position_threshold = 40  # Position error threshold in pixels (since we don't have real-world measurements now)
+
 
 def state_cb(msg):
     global current_state
     current_state = msg
 
+def image_callback(msg):
+    global marker_center
+    bridge = CvBridge()
+
+    # Convert ROS Image message to OpenCV image
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+    # Convert in gray scale
+    gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    local_gray_image = gray_image
+
+    # Load the predefined dictionary
+    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+
+    # Initialize the detector parameters using default values
+    parameters = cv2.aruco.DetectorParameters_create()
+    
+    # Increase the minimum and maximum marker area
+    # Increase the minimum and maximum marker perimeter rates
+    parameters.minMarkerPerimeterRate = 0.0001  # Lower value to be more lenient
+    parameters.maxMarkerPerimeterRate = 10.0   # Higher value to be more lenient
+
+
+    # Detect the markers in the image
+    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(gray_image, dictionary, parameters=parameters)
+
+    if markerIds is not None:
+        # We have detected at least one marker
+        # Get the center of the marker
+        center_x = np.mean(markerCorners[0][0,:,0])
+        center_y = np.mean(markerCorners[0][0,:,1])
+        rospy.loginfo("Marker detected")
+        # Update marker_center
+        marker_center = np.array([center_x, center_y])
+        centerObject(gray_image, marker_center)
+        
+def centerObject(gray_image, marker_center):
+    vel_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel_unstamped', Twist, queue_size=10)
+    velocity = Twist()
+    
+    # Calculate position error
+    error_x = gray_image.shape[1]/2 - marker_center[0]  # x error
+    error_y = gray_image.shape[0]/2 - marker_center[1]  # y error
+    
+    scaled_speed_x = calculate_scaled_speed(abs(error_x))
+    scaled_speed_y = calculate_scaled_speed(abs(error_y))
+
+    # X and Y control
+    if abs(error_x) > position_threshold:
+        if error_x > 0:
+            print("Error X: Moving Left " + str(scaled_speed_x))
+            velocity.linear.x = scaled_speed_x
+        else:
+            print("Error X: Moving Right " + str(scaled_speed_x))
+            velocity.linear.x = -scaled_speed_x
+    else:
+        velocity.linear.x = 0
+
+    if abs(error_y) > position_threshold:
+        if error_y > 0:
+            print("Error Y: Moving up" + str(error_y))
+            velocity.linear.y = scaled_speed_y
+        else:
+            print("Error Y: Moving down " + str(error_y))
+            velocity.linear.y = scaled_speed_y
+    else:
+        velocity.linear.y = 0
+    
+    vel_pub.publish(velocity)
+
+def calculate_scaled_speed(error):
+    if error > position_threshold:
+        # Calculate the scaled speed within the range of 0 to maximum_speed
+        scaled_speed = (error - position_threshold) / (horizontal_speed - position_threshold) * horizontal_speed
+        return scaled_speed
+    else:
+        return 0.0
 
 def callback(data):
     # Get pose data
@@ -23,25 +110,35 @@ def callback(data):
     altitude = pose.position.z
     print(altitude)
 
-    if altitude > takeoff_altitude:
-        # Create a publisher for the velocity commands
-        velocity_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
+    # Create a publisher for the velocity commands
+    velocity_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=10)
 
-        # Create a Twist message and set the linear and angular velocities
-        velocity_msg = TwistStamped()
-        velocity_msg.twist.linear.z = 1.0
+    # Create a Twist message and set the linear and angular velocities
+    velocity_msg = TwistStamped()
+        
+    if altitude > takeoff_altitude:
+        
+        velocity_msg.twist.linear.z = -1.0
 
         # Publish the velocity message repeatedly
         rate = rospy.Rate(10)  # 10 Hz
         velocity_msg.header.stamp = rospy.Time.now()
         velocity_pub.publish(velocity_msg)
         rate.sleep()
+    else:
+        velocity_msg.twist.linear.z
+        rate = rospy.Rate(10)  # 10 Hz
+        velocity_msg.header.stamp = rospy.Time.now()
+        velocity_pub.publish(velocity_msg)
+        rate.sleep()
+        
 
 def listener():
     rospy.init_node('listener', anonymous=True)
     arm()
     takeoff()
     rospy.Subscriber('/mavros/global_position/local', Odometry, callback)
+    image_sub = rospy.Subscriber("webcam/image_raw", Image, image_callback)
     rospy.spin()
 
 def arm():
--- scripts/tracking.py
+++ scripts/tracking.py
@@ -7,19 +7,20 @@ from geometry_msgs.msg import Twist
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, SetMode
 from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, Range
 from threading import Lock
 
 # Globals
 current_state = None
 marker_center = None
+gray_image=None
+current_altitude = None
 lock = Lock()
-gray = None
 
 # Camera calibration parameters
 # None in this case
 
-target_z = 5.0  # Target altitude in meters
+target_z = 4.0  # Target altitude in meters
 altitude_threshold = 0.1  # Altitude error threshold in meters
 vertical_speed = 0.5  # Vertical speed when correcting altitude
 
@@ -30,6 +31,10 @@ def state_cb(msg):
     global current_state
     current_state = msg
 
+# def altitude_callback(msg):
+#     global current_altitude
+#     current_altitude = msg.range
+
 def image_callback(msg):
     global marker_center, lock
     bridge = CvBridge()
@@ -38,7 +43,8 @@ def image_callback(msg):
     cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
     # Convert in gray scale
-    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    local_gray_image = gray_image
 
     # Load the predefined dictionary
     dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
@@ -47,24 +53,26 @@ def image_callback(msg):
     parameters = cv2.aruco.DetectorParameters_create()
 
     # Detect the markers in the image
-    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(gray, dictionary, parameters=parameters)
+    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(gray_image, dictionary, parameters=parameters)
 
     if markerIds is not None:
         # We have detected at least one marker
         # Get the center of the marker
         center_x = np.mean(markerCorners[0][0,:,0])
         center_y = np.mean(markerCorners[0][0,:,1])
-
+        rospy.loginfo("Marker detected")
         # Update marker_center
         with lock:
+            gray_image = local_gray_image
             marker_center = np.array([center_x, center_y])
 
 rospy.init_node('offb_node', anonymous=True)
 last_request = rospy.Time.now()
+rate = rospy.Rate(20.0)
 state_sub = rospy.Subscriber("mavros/state", State, state_cb)
 image_sub = rospy.Subscriber("webcam/image_raw", Image, image_callback)
+# altitude_sub = rospy.Subscriber("mavros/global_position/rel_alt", Range, altitude_callback)
 vel_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel_unstamped', Twist, queue_size=10)
-
 arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
 set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
 
@@ -74,10 +82,10 @@ while not rospy.is_shutdown():
     if current_state is not None and current_state.connected:
         if current_state.armed and current_state.mode == "GUIDED":
             with lock:
-                if marker_center is not None:
+                if marker_center is not None and gray_image is not None:
                     # Calculate position error
-                    error_x = gray.shape[1]/2 - marker_center[0]  # x error
-                    error_y = gray.shape[0]/2 - marker_center[1]  # y error
+                    error_x = gray_image.shape[1]/2 - marker_center[0]  # x error
+                    error_y = gray_image.shape[0]/2 - marker_center[1]  # y error
 
                     # X and Y control
                     if abs(error_x) > position_threshold:
@@ -95,10 +103,18 @@ while not rospy.is_shutdown():
                             velocity.linear.y = -horizontal_speed
                     else:
                         velocity.linear.y = 0
+                    
+                    # # Z control
+                    # if abs(target_z - current_altitude) > altitude_threshold:
+                    #     if target_z > current_altitude:
+                    #         velocity.linear.z = vertical_speed
+                    #     else:
+                    #         velocity.linear.z = -vertical_speed
+                    # else:
+                    #     velocity.linear.z = 0
 
                     # Publish velocity command
                     vel_pub.publish(velocity)
-
-rate = rospy.Rate(20)
-
-rate.sleep()
+                    
+        
+    rate.sleep()
